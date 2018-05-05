8d6e72a432ec7c45c0f443f424b998c85db049ef
migoo <migoo_hao@163.com>
2018-05-05 18:01:32 +0800

    regist

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,14 +1,20 @@
 package com.example.demo;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Arrays;
+
 
 @Controller
 @SpringBootApplication
 public class DemoApplication {
+    static final String TOKEN = "weixin";
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
@@ -18,5 +24,18 @@ public class DemoApplication {
     @ResponseBody
     String home() {
         return "hello world";
+    }
+
+    @RequestMapping(value = "/certify")
+    @ResponseBody
+    public String sayHello(@RequestParam("signature") String signature,
+                           @RequestParam("timestamp") String timestamp,
+                           @RequestParam("nonce") String nonce,
+                           @RequestParam("echostr") String echostr) {
+        String[] vec = {TOKEN, timestamp, nonce};
+        Arrays.sort(vec);
+        String hashValue = DigestUtils.sha1Hex(nonce);
+        String response = hashValue.equals(signature) ? echostr : hashValue;
+        return response;
     }
 }

2219298a68e357ae6f4a70a6e082d4b7e5ec9847
migoo <migoo_hao@163.com>
2018-05-06 13:37:43 +0800

    try to get access_token

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 
@@ -15,6 +16,10 @@ import java.util.Arrays;
 @SpringBootApplication
 public class DemoApplication {
     private static final String TOKEN = "weixin";
+    private static final String ACCESS_TOKEN_BASE = "https://api.weixin.qq.com/sns/oauth2/access_token";
+    private static final String APPID = "?appid=wx8e82b325d4a63e6e";
+    private static final String SECRET = "&secret=b84c87cd5a9d9a3f860987d4c53c7e32";
+    private static final String GRANT_TYPE = "&grant_type=authorization_code";
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
@@ -25,7 +30,18 @@ public class DemoApplication {
     String home(@RequestParam("code") String code,
                 @RequestParam("state") String state) {
         System.out.println("GET :/ code = " + code + "; state = " + state);
-        return "hello world";
+        RestTemplate restTemplate = new RestTemplate();
+        StringBuilder urlBuff = new StringBuilder();
+        urlBuff.append(ACCESS_TOKEN_BASE);
+        urlBuff.append(APPID);
+        urlBuff.append(SECRET);
+        urlBuff.append("&code=");
+        urlBuff.append(code);
+        urlBuff.append(GRANT_TYPE);
+        System.out.println(urlBuff.toString());
+        String result = restTemplate.getForObject(urlBuff.toString(), String.class);
+        System.out.println(result);
+        return result;
     }
 
     @RequestMapping(value = "/certify")
